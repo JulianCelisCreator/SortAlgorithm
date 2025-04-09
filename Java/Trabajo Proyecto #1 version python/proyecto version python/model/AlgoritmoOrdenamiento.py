36fc54e18da51e49ad083ca8b13a2dcc340bb8f6
Julian David Celis Giraldo <103604347+JulianCelisCreator@users.noreply.github.com>
2025-04-09 01:51:27 -0500

    Delete Java/Trabajo Proyecto #1 version python directory

--- Java/Trabajo Proyecto #1 version python/proyecto version python/model/AlgoritmoOrdenamiento.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-""" Clase AlgoritmoOrdenamiento se encarga de crear el metodo ordenar
-    para aplicarlo en los tipos de ordenamiento disponibles"""
-
-class AlgoritmoOrdenamiento(ABC):
-    @abstractmethod
-    def ordenar(self, politicos, criterio='dinero'):
-        pass
